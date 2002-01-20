219ec4124068b586192a18fe2cf51ef4eb33c22e
inoue <inoue>
2002-01-20 18:57:04 +0000

    Initial revision

---
+++ lisp/emacspeak-m17n-ja.el
@@ -0,0 +1,302 @@
+;;; emacspeak-m17n-ja.el --- Bilingual extension to emacspeak
+;;; $Id: emacspeak-m17n-ja.el,v 1.1 2002/01/20 18:57:43 inoue Exp $
+;;; $Author: inoue $
+;;; Description: Contains functions that handle Japanese characters
+;;; Keywords: Emacspeak, Japanese, multilingualization
+;;;
+;;; This file is not part of Emacspeak
+
+;;;{{{  Copyright:
+;;;Copyright (C) 2000, Takayuki Watanabe (takayuki@la.shonan-it.ac.jp)
+;;; All Rights Reserved.
+;;;
+;;; This file is not part of GNU Emacs, but the same permissions apply.
+;;;
+;;; GNU Emacs is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 2, or (at your option)
+;;; any later version.
+;;;
+;;; GNU Emacs is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Emacs; see the file COPYING.  If not, write to
+;;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+
+;;;}}}
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;{{{  Introduction:
+
+;;; Commentary:
+
+;;; BEP, Bilingual Emacspeak Project, extends Emacspeak to bilingual, 
+;;; Japanese and English.
+;;; Japanese characters shoud be encoded with Shift JIS.
+;;; ShiftJIS is double-byte character.
+
+;;}}}
+;;{{
+(eval-when (compile)
+  (require 'cl)
+  (require 'emacspeak-ja-tbl))
+;;}}
+;;{{{
+;; Auditory-disp-table-ja and its access functions
+;; This table has 5 fields:
+;; 1) Key: Kanji or other 2-byte Japanese char "亜"
+;; 2) Value 1: read one char according to a cursor movement "あ"
+;; 3) Value 2: explain Kanji to identify it while Kana-Kanji translation "あじあ"
+;; 4) Value 3: almost same as 3) but explain Kanji in detail.
+;; 5) Symbol represents type of the character.
+;; The table provided with this package is based on GrassRoots,
+;; a Japanese screen reader for DOS
+;; developed by Jun Ishikawa <ishikawa@u-shizuoka-ken.ac.jp>
+
+(defvar auditory-display-table-ja
+  (make-char-table 'auditory-display-table)
+  "char-table from a Japanese character and corresponding explanatory readings")
+
+(defun bep-get-phonetic-string (char &optional field)
+  "Return the phonetic string, the 2nd field of dic, for this CHAR."
+  (interactive "sChar:\nnField:")
+  (let ((char (or (and (integerp char) char)
+		  (string-to-char char)))
+	(dummy-array ["dummy" "dummy" "dummy"]))
+    (or (aref
+	 (or
+	  (aref auditory-display-table-ja char)
+	  (fillarray dummy-array (char-to-string char))) ; avoid error and return char itself
+	 (if field field 0))
+	" ")))
+
+(defun bep-get-char-type (char)
+  "Return the symbol of char-type for this CHAR."
+  (let ((char (or (and (integerp char) char)
+		  (string-to-char char))))
+  (aref
+   (or
+    (aref auditory-display-table-ja char)
+    [nil nil nil nil])
+   3)))
+
+(defun bep-get-cursor-string (char)
+  "Return the short phonetic string (2nd field) of CHAR."
+  (interactive "sChar:\n")
+  (bep-get-phonetic-string char 0))
+
+(defun bep-get-explanatory-string (char)
+  "Return the explanatory string (3rd field) of CHAR."
+  (interactive "sChar:\n")
+  (bep-get-phonetic-string char 1))
+
+(defun bep-get-long-explanatory-string (char)
+  "Return the detail explanatory string (4th field) of CHAR."
+  (interactive "sChar:\n")
+  (bep-get-phonetic-string char 2))
+;;}}}
+;;{{
+(defvar emacspeak-ja-type-prefix-table
+  '(
+    (ja-hiragana . "ヒラガナ")
+    (ja-katakana . "カタカナ")
+    (ja-alpha . "ゼンカクローマジ")
+    (katakana . "ハンカク")
+    (ja-other . "ゼンカク"))
+  "Prefix to tell categories of Japanese characters.")
+
+(defvar emacspeak-ja-subtype-prefix-table
+  '(
+    (upcase . "オオモジ")
+    (small . "チイサイ"))
+  "Prefix to tell attributes of Japanese characters.")
+
+(defvar emacspeak-ja-small-char-list
+  '(?ぁ ?ぃ ?ぅ ?ぇ ?ぉ ?っ ?ゃ ?ゅ ?ょ ?ァ ?ィ ?ゥ ?ェ ?ォ ?ッ ?ャ ?ュ ?ョ
+	?ｱ ?ｲ ?ｳ ?ｴ ?ｵ ?ﾔ ?ﾕ ?ﾖ)
+  "List of Japanese characters which has attribute `small'.")
+;;}}
+;;{{{ Other Japanese specific functions
+
+(defun emacspeak-ja-convert-string-to-phonetic (str)
+  (let ((cur 0)
+	(cidx 0)
+	(siz (length str))
+	(ret ""))
+    (while (< cur siz)
+      (let* ((c (aref str cur))
+	     (ct (emacspeak-ja-get-char-type c))
+	ptr)
+	;;; get the end of region with current char type
+	(setq cidx (1+ cur))
+	(while (and (< cidx siz)
+		    (eq ct (emacspeak-ja-get-char-type
+			    (aref str cidx))))
+	  (setq cidx (1+ cidx)))
+	;;; make phonetic string
+	(setq ret (concat ret
+			  (or (cdr
+			       (assoc ct emacspeak-ja-type-prefix-table))
+			       "")
+			   ))
+	(setq ptr cur)
+	(while (< ptr cidx)
+	  (let* ((cptr (aref str ptr))
+		 (cstr (char-to-string cptr))
+		 (phon (if (memq ct '(ja-hiragana ja-katakana katakana ja-alpha alpha))
+			   (bep-get-cursor-string cstr)
+			   (bep-get-explanatory-string cstr)))
+		 (subtype (emacspeak-ja-get-char-subtype cptr))
+		 (pref (or (cdr (assoc subtype emacspeak-ja-subtype-prefix-table)) ""))
+		 )
+	    (setq ret (concat ret pref phon)))
+	  (if (and (< ptr (1- siz)) (eq ct 'ja-kanji))
+	      (setq ret (concat ret " ")))
+	  (setq ptr (1+ ptr))
+	  )
+	(setq cur cidx)
+	)
+      )
+    ret
+    )
+)
+
+(defun emacspeak-ja-convert-char-to-phonetic (cstr)
+  "convert first character of string to explanatory string with prefix indicating character subtype."
+  (let* ((cptr (or (and (integerp cstr) cstr)
+		   (string-to-char cstr)))
+	 (phon (bep-get-explanatory-string cptr))
+	 (ct (emacspeak-ja-get-char-type cptr))
+	 (subtype (emacspeak-ja-get-char-subtype cptr))
+	 (pref 
+	  (or (cdr
+	       (assoc ct emacspeak-ja-type-prefix-table))
+	      "")
+	  )
+	 (spref (or (cdr (assoc subtype emacspeak-ja-subtype-prefix-table)) ""))
+	 ret)
+    (setq ret (concat ret pref spref phon))
+    ret)
+)
+
+(defun emacspeak-ja-convert-string-to-cursor (str)
+  "convert string to concatenated string of cursor-string"
+  (let ((siz (length str))
+	     (output "")
+	     (ptr 0))
+    (while (< ptr siz)
+      (setq cstr (substring str ptr (1+ ptr)))
+      (setq output (concat output (bep-get-cursor-string cstr)))
+      (setq ptr (1+ ptr))
+      )
+  output
+))
+
+(defun emacspeak-ja-get-char-type (c)
+  "returns Japanese character type"
+  (let ((ct (bep-get-char-type c))
+	(cstr (char-to-string c)))
+  (cond
+   (ct ct) ; if char-type is defined in auditory-disp-table-ja
+   ((string-match "[0-9]" cstr) 'number)
+   ((string-match "[A-Za-z]" cstr) 'alpha)
+   ((string-match "[ｦ-ﾟ]" cstr) 'katakana)
+   (t nil))
+))
+
+(defun emacspeak-ja-get-char-subtype (c)
+  "returns subtype of c"
+  (cond
+   ((or
+     (and (>= c ?A) (<= c ?Z))
+     (and (>= c ?Ａ) (<= c ?Ｚ))
+     (and (>= c ?Α) (<= c ?Ω)))
+    'upcase)
+   ((memq c emacspeak-ja-small-char-list)
+    'small)
+   (t nil)
+   )
+)
+
+(defun bep-is-Japanese-p (char-string)
+  "return True if Japanese, i.e. double-byte character"
+  (interactive)
+   (equal 0 (string-match "\\cj" char-string)) ; if Japanese characters
+)
+;;}}}
+;;{{{ Fix brackets some characters for Japanese output.
+(defsubst dtk-fix-characters-ja (mode)
+  "Quote any delimiters that need special treatment.
+Argument MODE  specifies the current pronunciation mode.
+Japanese specific."
+  (declare  (special dtk-fix-characters-regexp-ja
+		     dtk-replace-characters-table-ja))
+  (goto-char (point-min))
+  (cond
+   ((string=  "all"  mode )
+    (let ((start nil)
+          (personality nil)
+	  (language nil)
+	  (c nil))
+      (while (re-search-forward dtk-fix-characters-regexp-ja nil t )
+        (setq start (1- (point)))
+        (setq personality
+              (get-text-property
+               start 'personality))
+	(setq language
+	      (get-text-property
+	       start 'emacspeak-language))
+        (cond
+         ((setq c (assoc (char-to-string (char-after (match-beginning 0 ))) dtk-replace-characters-table-ja))
+          (replace-match (cdr c))
+	  (when personality
+	    (put-text-property start (point)
+			       'personality personality))
+	  (when language
+	    (put-text-property start (point)
+			       'emacspeak-language language)))
+	 (t
+	  (replace-match " "))))))
+   (t
+    (while (re-search-forward dtk-fix-characters-regexp-ja  nil t )
+      (let ((language (get-text-property (match-beginning 0)
+					 'emacspeak-language)))
+      (replace-match " " nil t )
+      (put-text-property (1- (point)) (point)
+			     'emacspeak-language language))
+      ))))
+;;}}}
+;;{{ Japanese specific put-language functions
+(defun emacspeak-m17n-put-language-ja-ne (beg end)
+  "Put `ja' Property to Japanese characters,
+and `en' property to ASCII characters."
+  (goto-char beg)
+  (while (and (< (point) end)
+	      (re-search-forward "\\([\011\012\014 -~]+\\|\\(\\cj\\|\n\\)+\\)" end t))
+    (let* ((mbeg (match-beginning 0))
+	   (mend (match-end 0))
+	   (beginning (= mbeg (point-min)))
+	   (ending (= mend (point-max)))
+	   (matched 
+	    (buffer-substring mbeg mend)))
+      (cond
+       ((string-match "^\\cj" matched)
+	(put-text-property mbeg mend 'emacspeak-language 'ja))
+       (t
+	(put-text-property mbeg mend 'emacspeak-language 'en))
+       ) ; end of cond
+      )))
+;;}}
+
+(provide 'emacspeak-m17n-ja)
+;;{{{ end of file
+
+;;; local variables:
+;;; folded-file: t
+;;; byte-compile-dynamic: t
+;;; end:
+
+;;}}}
---
+++ lisp/emacspeak-m17n-setup.el
@@ -0,0 +1,231 @@
+;;; emacspeak-m17n-setup.el --- emacspeak-m17n-setup: set up for Multilingual support in Emacspeak.
+
+;; Copyright (C) 2001  Free Software Foundation, Inc.
+
+;; Author: Koichi INOUE <inoue@argv.org>
+;; Keywords: i18n, multimedia, extensions
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;; Loads and set up everything needed for m17n support.
+
+;;; Code:
+
+;;{{ Variables
+(defvar emacspeak-m17n-auto-put-language-mode nil
+  "If t, guess language of characters automatically.")
+
+(put 'auditory-display-table 'char-table-extra-slots 6)
+
+(defvar emacspeak-display-table-alist nil
+  "Map from language to auditory-display-table.\n
+Car of each cell is naming symbol of language and cdr is corresponding\n
+auditory-display-table.")
+;;}}
+;;{{ Generic m17n support functions
+(defsubst emacspeak-m17n-register-display-table (lang table)
+  "Register the association of language and auditory-display-table name."
+  (setq emacspeak-display-table-alist
+	(append (cons (cons lang table) nil)
+		emacspeak-display-table-alist)))
+
+(defsubst emacspeak-get-display-table (lang)
+  "Get auditory-display-table corresponding to language."
+  (let ((tab (assq lang emacspeak-display-table-alist)))
+    (cdr tab)))
+
+(defun emacspeak-m17n-get-phonetic-string (char lang)
+  "Get phonetic string of char according to its language."
+  (let ((table (emacspeak-get-display-table lang)))
+    (cond
+     ((and (eq lang 'ja) (featurep 'emacspeak-m17n-ja))
+      (emacspeak-ja-convert-char-to-phonetic char))
+     (t (char-to-string char)))
+))
+
+(defun emacspeak-m17n-get-cursor-string (char lang)
+  "Get string for speech with cursor movement according to its language."
+  (let ((table (emacspeak-get-display-table lang)))
+    (cond
+     ((and (eq lang 'ja) (featurep 'emacspeak-m17n-ja))
+       (bep-get-phonetic-string char))
+     (t (char-to-string char)))
+))
+;;}}
+;;{{
+(defun emacspeak-m17n-put-language-region (beg end &optional len)
+  "Put language properties according to current language configuration."
+  (interactive "r")
+  (let ((inhibit-read-only t) (buffer-undo-list t)
+	(modified (buffer-modified-p))
+	befor-change-functions after-change-functions
+	put-language-func)
+    (setq put-language-func 'emacspeak-m17n-put-language-ja-ne)
+    (unwind-protect
+	(save-match-data
+	  (save-excursion
+	    (funcall put-language-func beg end)))
+      ;;; Clean up
+      (and (not modified) (buffer-modified-p) (set-buffer-modified-p nil)))))
+
+(defun emacspeak-m17n-put-language-internal (beg end &optional len)
+  "Put language properties according to current language configuration."
+  (interactive "r")
+  (declare (special emacspeak-m17n-auto-put-language-mode))
+  (when (and emacspeak-m17n-auto-put-language-mode
+	     (emacspeak-buffer-visible-p))
+  (let ((inhibit-read-only t) (buffer-undo-list t)
+	(modified (buffer-modified-p))
+	befor-change-functions after-change-functions
+	(begm (max (window-start) beg))
+	(endm (min (window-end) end))
+	put-language-func)
+    (setq put-language-func 'emacspeak-m17n-put-language-ja-ne)
+    (unwind-protect
+	(save-match-data
+	  (save-excursion
+	    (funcall put-language-func begm endm)))
+      ;;; Clean up
+      (and (not modified) (buffer-modified-p) (set-buffer-modified-p nil))))))
+
+;;}}
+;;{{ Put language property
+
+(defun emacspeak-m17n-auto-put-language-mode (&optional arg)
+  "Toggle auto-put-language-mode.
+Enabled if prefix argument is positive, and disabled
+if negative."
+  (interactive "P")
+  (set (make-local-variable 'emacspeak-m17n-auto-put-language-mode)
+       (if arg (> (prefix-numeric-value arg) 0)
+	 (not emacspeak-m17n-auto-put-language-mode)))
+  (cond
+   (emacspeak-m17n-auto-put-language-mode
+    (emacspeak-m17n-put-language-install))
+   (t
+    (emacspeak-m17n-put-language-uninstall)))
+  (when (interactive-p)
+    (dtk-speak (concat "Turned " (if emacspeak-m17n-auto-put-language-mode "on" "off")
+	       "auto language assignment"))))
+
+;; provide language property automatically.
+;; Idea is the same as lazy-voice-lock-mode uses.
+(defun emacspeak-m17n-put-language-install ()
+  (add-hook 'after-change-functions 'emacspeak-m17n-put-language-internal nil t)
+  (add-hook 'window-scroll-functions 'emacspeak-m17n-put-language-after-scroll nil t)
+  (add-hook 'before-change-functions 'emacspeak-m17n-put-language-arrange-before-change nil t))
+
+(defun emacspeak-m17n-put-language-uninstall ()
+  (remove-hook 'after-change-functions 'emacspeak-m17n-put-language-internal)
+  (remove-hook 'window-scroll-functions 'emacspeak-m17n-put-language-after-scroll)
+  (remove-hook 'before-change-functions 'emacspeak-m17n-put-language-arrange-before-change))
+
+(defun emacspeak-m17n-put-language-after-scroll (window window-start)
+  ;; Called from `window-scroll-functions'.
+  ;; Borrowed from `lazy-voice-lock.el'.
+  (save-excursion
+    (goto-char window-start)
+    (vertical-motion (window-height window) window)
+    (emacspeak-m17n-put-language-region window-start (point)))
+  (set-window-redisplay-end-trigger window nil))
+
+(defun emacspeak-m17n-put-language-after-trigger (window trigger-point)
+  ;; Called from `redisplay-end-trigger-functions'.
+  ;; Borrowed from `lazy-voice-lock.el'.
+  (save-excursion
+    (goto-char (window-start window))
+    (vertical-motion (window-height window) window)
+    (emacspeak-m17n-put-language-region trigger-point (point))))
+
+(defun emacspeak-m17n-put-language-after-resize (frame)
+  ;; Called from `window-size-change-functions'.
+  ;; Borrowed from `lazy-voice-lock.el'.
+  (save-excursion
+    (save-selected-window
+      (select-frame frame)
+      (walk-windows (function (lambda (window)
+		       (set-buffer (window-buffer window))
+		       (emacspeak-m17n-put-language-region
+			(window-start window) (window-end window))
+		       (set-window-redisplay-end-trigger window nil)))
+		    'nomini frame))))
+
+(defun emacspeak-m17n-put-language-arrange-before-change (beg end)
+  ;; Called from `before-change-functions'.
+  ;; Borrowed from `lazy-voice-lock.el'.
+  (unless (eq beg end)
+    (let ((windows (get-buffer-window-list (current-buffer) 'nomini t)) window)
+      (while windows
+	(setq window (car windows))
+	(unless (markerp (window-redisplay-end-trigger window))
+	  (set-window-redisplay-end-trigger window (make-marker)))
+	(set-marker (window-redisplay-end-trigger window) (window-end window))
+	(setq windows (cdr windows))))))
+;;}}
+;;{{ Helper functions
+(defun emacspeak-buffer-visible-p (&optional buffer)
+  "See whether current buffer is visible.
+If visible on some window, return the window.
+If BUFFER is not specified, see if currentbuffer is visible."
+  (let ((ret nil)
+	(buf (if buffer buffer (current-buffer))))
+    (walk-windows
+     (function (lambda (window)
+	(when (eq buf (window-buffer window))
+	  (setq ret window))))
+     nil t)
+    ret))
+
+(defun emacspeak-buffer-portion-visible-p (beg end &optional buffer)
+  "See whether specified portion of current buffer is visible.
+If visible on some window, return the window.
+If BUFFER is not specified, see if currentbuffer is visible."
+  (let (ret
+	(buf (if buffer buffer (current-buffer))))
+    (walk-windows
+     (function (lambda (window)
+	(when (eq buf (window-buffer window))
+	  (setq ret window))))
+     nil t)
+    (if (and (> beg (window-start ret))
+	     (< end (window-end ret)))
+	ret)))
+
+(defun emacspeak-m17n-maybe-last-input-language ()
+  "Return language of maybe-last-input character or default-language"
+  (let (lang)
+    (and (> (point) (point-min))
+	 (setq lang (get-text-property (1- (point)) 'emacspeak-language)))
+    (or lang
+	dtk-default-language)
+))
+
+;;}}
+;;{{ Global Initialization
+(require 'emacspeak-m17n-ja)
+(require 'emacspeak-ja-tbl)
+
+(add-hook 'after-change-functions 'emacspeak-m17n-put-language-internal)
+(add-hook 'window-scroll-functions 'emacspeak-m17n-put-language-after-scroll)
+(add-hook 'before-change-functions 'emacspeak-m17n-put-language-arrange-before-change)
+(setq emacspeak-m17n-auto-put-language-mode t)
+;;}}
+;;{{ Final setup
+;;}}
+(provide 'emacspeak-m17n-setup)
+;;; emacspeak-m17n-setup.el ends here
